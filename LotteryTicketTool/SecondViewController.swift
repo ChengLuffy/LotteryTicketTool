b402688d6bff884d9665484fc5b017cfbb6e887f
成殿 <chengluffy@hotmail.com>
2018-11-12 16:36:02 +0800

    Initial Commit

---
+++ LotteryTicketTool/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LotteryTicketTool
+//
+//  Created by 成殿 on 12/11/2018.
+//  Copyright © 2018 chengluffy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
